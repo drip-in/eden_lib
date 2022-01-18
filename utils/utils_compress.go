bfb797094263141d3968fca36d1ffbdce6bbf29a
hudingyu <hudingyu.virgil@bytedance.com>
2022-01-18 11:26:42 +0800

    feat: add utils
    Change-Id: I5b6f7b665aa174bcfa23f9e2a11b829f78805e78

---
+++ utils/utils_compress.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io/ioutil"
+)
+
+//gzip压缩
+func GzipEncode(in []byte) (ret []byte, err error) {
+	var buffer bytes.Buffer
+	writer := gzip.NewWriter(&buffer)
+	defer writer.Close()
+	_, err = writer.Write(in)
+	if err != nil {
+		return
+	}
+	return buffer.Bytes(), nil
+}
+
+//gzip解压缩
+func GzipDecode(data []byte) (ret []byte, err error) {
+	reader, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return
+	}
+	defer reader.Close()
+	return ioutil.ReadAll(reader)
+}
